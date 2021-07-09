62f71c65cc4d6ac48753a445787b1c7e851390be
Chirag Chauhan <chiragvchauhan93@gmail.com>
2021-07-09 11:00:14 +0530

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import "zone.js"; // Required for Angular
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+Office.initialize = () => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}
